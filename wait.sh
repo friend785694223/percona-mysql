0b48d0f90db91edcdaed3d7e21c1d44807969fc8
William Hanwoody <hanwoody@gmail.com>
2014-07-09 11:49:47 +0800

    increase count 50 to 5000

--- wait.sh
+++ wait.sh
@@ -2,12 +2,12 @@
 
 [ -z "$1" ] && echo "Usage: $0 'command'" && exit 1
 count=0
-while [ $count -lt 50 ]; do
+while [ $count -lt 5000 ]; do
   mysql -e "show status" > /dev/null 2>&1 && break
   sleep 0.2
   (( count++ ))
 done
 
-[ $count -eq 50 ] && echo "wait mysqld more than 10s, timeout." && exit 1
+[ $count -eq 5000 ] && echo "wait mysqld more than 1000s, timeout." && exit 1
 
 eval "$1"
